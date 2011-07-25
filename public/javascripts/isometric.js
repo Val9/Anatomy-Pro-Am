84902462408dbc0fa830b5bceb66765228adaec5
Joseph Snell <jlsnell@wisc.edu>
2011-07-25 15:41:16 -0500

    now different room views are in seperate html files, the corresponding js would be nice to put in seperate js files but that is a low priority, next priority is having multiple people in the same environment

--- public/javascripts/isometric.js
+++ public/javascripts/isometric.js
@@ -3,156 +3,136 @@ components.isometricView = function(){
 	window.IsometricView = Backbone.View.extend({
 		el: $('#game'),
 		events: {
-			"click #background_Office": "objectClicked",
-			"click #whiteboard": "objectClicked",
-			"click #computerDesk": "objectClicked",
-			"click #message_help": "objectClicked",
-			"click #exitRoom": "objectClicked",
-			"click #yes_selected":"objectClicked",
-			"click #no_selected":"objectClicked"
+			"click .clickable": "objectClicked"
 		},
 		initialize: function() {
-			_.bindAll(this, 'render');
-			this.render();
+			_.bindAll(this, 'loadRoom');
+			this.loadRoom(1);
 		},
 		render: function() {
-			$.get('/renders/isometric.html', function(t){
-				this.el.html('');
-				view.isometric = t;
-				this.el.html(view.isometric);
-				this.setupView();
-				console.log("Here");
-			}.bind(this));
+			
 		},
 		setupView: function() {
-			this.loadRoom(2);
+			this.loadRoom(1);
 		},
 		loadRoom: function(roomNum) {
 			this.roomNum = roomNum;
-			(function($){ 
-				switch(roomNum){
-					case 1:
-						$('#room_div').html('<img src="images/doctors_office.png" id="background_Office" style="position:absolute; left:90px; top:170px;"/>'	
-						+'<img src="images/computerDeskDecoy.png" id="computerDesk" style="position:absolute; left:410px; top:335px;"/>'
-						+'<img src="images/whiteboardDecoy.png" id="whiteboard" style="position:absolute; left:140px; top:220px;"/>'
-						+'<img src="images/exitRoomObject.png" id="exitRoom" style="position:absolute; left:345px; top:195px;"/>');
-						$('#image_div').html("<img src='images/def_head.png'/>");
-						$('#grid').iso({
-						    unwalkables: [
-								"3_0_0",
-								"4_0_0",
-								"5_0_0",
-								"6_0_0",
-								"7_0_0",
-								"8_0_0",
-						      	"9_0_0",
-								"3_1_0",
-								"4_1_0",
-								"5_1_0",
-								"6_1_0",
-								"7_1_0",
-								"8_1_0",
-						      	"9_1_0",
-								"3_4_0",
-								"3_5_0",
-								"4_3_0",
-								"4_4_0",
-						      	"4_5_0",
-						      	"4_6_0",
-						      	"4_7_0",
-								"5_3_0",
-						      	"5_4_0",
-								"5_5_0",
-								"5_6_0",
-								"5_7_0",
-								"6_4_0",
-								"6_5_0",
-								"6_6_0",
-								"6_7_0",
-								"7_7_0",
-								"7_6_0",
-								"7_5_0"
-						    ], 
-						    max_x: 10, 
-						    max_y: 10,
-						    avatar_offset: [4,-110],
-							shadow_offset: [4,-8],
-							tile_width:48,
-						    tile_height:26,
-						    iso_tile_height:26
-	
-						  });
+			switch(roomNum){
+				case 1:
+					$.get('/renders/rooms/doctorsOffice.html', function(t){
+						this.el.html('');
+						this.el.html(t);
+						(function($){ 
+							$('#grid').iso({
+							    unwalkables: [
+									"3_0_0",
+									"4_0_0",
+									"5_0_0",
+									"6_0_0",
+									"7_0_0",
+									"8_0_0",
+							      	"9_0_0",
+									"3_1_0",
+									"4_1_0",
+									"5_1_0",
+									"6_1_0",
+									"7_1_0",
+									"8_1_0",
+							      	"9_1_0",
+									"3_4_0",
+									"3_5_0",
+									"4_3_0",
+									"4_4_0",
+							      	"4_5_0",
+							      	"4_6_0",
+							      	"4_7_0",
+									"5_3_0",
+							      	"5_4_0",
+									"5_5_0",
+									"5_6_0",
+									"5_7_0",
+									"6_4_0",
+									"6_5_0",
+									"6_6_0",
+									"6_7_0",
+									"7_7_0",
+									"7_6_0",
+									"7_5_0"
+							    ], 
+							    max_x: 10, 
+							    max_y: 10,
+							    avatar_offset: [4,-110],
+								shadow_offset: [4,-8],
+								tile_width:48,
+							    tile_height:26,
+							    iso_tile_height:26
+							  });
 				
-						  $('#grid').children().mouseenter(function(e) {
-						    $(e.target).addClass('hover');
-						  });
+							  $('#grid').children().mouseenter(function(e) {
+							    $(e.target).addClass('hover');
+							  });
 
-						  $('#grid').children().mouseleave(function(e) {
-						    $(e.target).removeClass('hover');
-						  });
+							  $('#grid').children().mouseleave(function(e) {
+							    $(e.target).removeClass('hover');
+							  });
+							})(jQuery);
+						}.bind(this));
 						break;
-					case 2:
-						$('#room_div').html('<img src="images/room3.png" id="background_Office" style="position:absolute; left:20px; top:120px;"/>');
-						$('#grid').css({top:"100px", left:"170px"});
-						$('#image_div').html("<img src='images/def_head.png'/>");
-						$('#message_info_text').html("This is to show that switching between areas is easy,"
-						+" feel free to move around here, the goal was to make the stairs work right, unfortunately they don't.");
+				case 2:
+					$.get('/renders/rooms/room2.html', function(t){
+						this.el.html('');
+						this.el.html(t);
+						(function($){
+							var unwalkables = new Array();
+							var elevateds = {};
 						
-						var unwalkables = new Array();
-						var elevateds = {};
-						
-						for(var x = 0; x < 10; x++)
-							for(var y = 0; y<13; y++){
-								if(x==9 && y<7){
-									if(y>2){
-										console.log(y);
-										elevateds[""+x+"_"+y]=(7-y);
-									}else{ 
-										console.log("is fired");
-										elevateds[""+x+"_"+y]=4;
+							for(var x = 0; x < 10; x++)
+								for(var y = 0; y<13; y++){
+									if(x==9 && y<7){
+										if(y>2){
+											console.log(y);
+											elevateds[""+x+"_"+y]=(7-y);
+										}else{ 
+											console.log("is fired");
+											elevateds[""+x+"_"+y]=4;
+										}
+									}
+									if(((x<4&&y<4) || y<2) || ((x>5&&x<9) && y<7)){
+										if(elevateds[""+x+"_"+y])
+											unwalkables.push(""+x+"_"+y+"_"+elevateds[""+x+"_"+y]);
+										else
+											unwalkables.push(""+x+"_"+y+"_0");
 									}
 								}
-								if(((x<4&&y<4) || y<2) || ((x>5&&x<9) && y<7)){
-									if(elevateds[""+x+"_"+y])
-										unwalkables.push(""+x+"_"+y+"_"+elevateds[""+x+"_"+y]);
-									else
-										unwalkables.push(""+x+"_"+y+"_0");
-								}
-							}
-						console.log("stop");
-						console.log(unwalkables);
-						console.log(elevateds);
-						console.log("end");
+							console.log("stop");
+							console.log(unwalkables);
+							console.log(elevateds);
+							console.log("end");
 						
-						$('#grid').iso({
-							elevateds: elevateds,
-							unwalkables: unwalkables,
-						    max_x: 10, 
-						    max_y: 13,
-							startPosition: [9,4,3],
-						    avatar_offset: [4,-110],
-							shadow_offset: [4,-8],
-							tile_width:48,
-						    tile_height:26,
-						    iso_tile_height:26
-						  });
+							$('#grid').iso({
+								elevateds: elevateds,
+								unwalkables: unwalkables,
+							    max_x: 10, 
+							    max_y: 13,
+								startPosition: [9,4,3],
+							    avatar_offset: [4,-110],
+								shadow_offset: [4,-8],
+								tile_width:48,
+							    tile_height:26,
+							    iso_tile_height:26
+							  });
 							
-						  $('#grid').children().mouseenter(function(e) {
-						    $(e.target).addClass('hover');
-						  });
+							  $('#grid').children().mouseenter(function(e) {
+							    $(e.target).addClass('hover');
+							  });
 
-						  $('#grid').children().mouseleave(function(e) {
-						    $(e.target).removeClass('hover');
-						  });
-						
-					   	
-					    console.log(jQuery.fn.iso.nodes);
-						
-						
-						break;
-				}
-			})(jQuery);
-			
+							  $('#grid').children().mouseleave(function(e) {
+							    $(e.target).removeClass('hover');
+							  });
+						})(jQuery);
+				}.bind(this));
+				break;
+			}
 		},
 		objectClicked: function(e) {
 			console.log(e.target.id);
@@ -180,8 +160,8 @@ components.isometricView = function(){
 					$('#image_div').html("<img src='images/def_head.png'/>");
 			}else if(e.target.id == "exitRoom"){
 					$('#message_info_text').html("Are you sure you want to leave this room?"
-					+"<div class='message_info_header' id='yes_selected'>YES</div>"
-					+"<div class='message_info_header' id='no_selected'>NO</div>");
+					+"<div class='message_info_header clickable' id='yes_selected'>YES</div>"
+					+"<div class='message_info_header clickable' id='no_selected'>NO</div>");
 					$('#image_div').html("<img src='images/def_head.png'/>");
 			}else if(e.target.id == "yes_selected"){
 					$('#message_info_text').html("Leaving room");
@@ -190,15 +170,6 @@ components.isometricView = function(){
 			}else{
 				console.log("There's nothing there!");
 			}
-			/*
-			function() {
-		        $('#information').html("");
-		        $('li a').removeClass("selected");
-		        $(this).addClass("selected");
-		        $("#demo").load(e.currentTarget.href, function() {
-		        	$('#information').fadeIn("fast");
-				});
-			});*/
 		}
 	});
 };
